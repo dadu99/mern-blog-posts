307511f1008e3945dc905a83f9361c96137e33ea
Darius Baciu <baciu.darius01@gmail.com>
2024-03-15 16:40:05 +0200

    first commit api + client && first page

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Post from "./Post";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          My Blog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,29 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2022/07/GettyImages-1235254642.jpg?w=430&h=230&crop=1"
+          alt="post"
+        ></img>
+      </div>
+
+      <div className="texts">
+        <h2>
+          Apple acquires AI startup specializing in overlooking manufacturing
+          components
+        </h2>
+        <p className="info">
+          <a className="author">Dadu</a>
+          <time>2024-03-15 16:45</time>
+        </p>
+        <p className="summary">
+          Apple has added another AI startup to its acquisition list with
+          Canada-based DarwinAI, which specializes in vision-based tech to
+          observe components during manufacturing to improve efficiency,
+          Bloomberg reported.
+        </p>
+      </div>
+    </div>
+  );
+}
