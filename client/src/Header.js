540bd6615a5f9672b4280d06c5fe52c2abfb6c79
Darius Baciu <baciu.darius01@gmail.com>
2024-03-20 14:35:21 +0200

    usercontext for update header and refresh after login

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
-  const [username, setUsername] = useState("");
-
+  const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
     fetch("http://localhost:4000/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
-        setUsername(userInfo.username);
+        setUserInfo(userInfo);
       });
     });
   }, []);
@@ -19,18 +19,21 @@ export default function Header() {
       credentials: "include",
       method: "POST",
     });
-    setUsername(null);
+    setUserInfo(null);
   }
+
+  const username = userInfo?.username;
+
   return (
     <header>
-      <Link href="/" className="logo">
-        My Blog
+      <Link to="/" className="logo">
+        MyBlog
       </Link>
       <nav>
         {username && (
           <>
             <Link to="/create">Create new post</Link>
-            <div onClick={logout}>Logout</div>
+            <a onClick={logout}>Logout ({username})</a>
           </>
         )}
         {!username && (
