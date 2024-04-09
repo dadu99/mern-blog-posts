3b16219843a06d40788bcbc576cf8a379954722e
Darius Baciu <baciu.darius01@gmail.com>
2024-04-09 14:13:28 +0300

    unique key id prop

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -7,11 +7,17 @@ export default function IndexPage() {
   useEffect(() => {
     fetch("http://localhost:4000/post").then((response) => {
       response.json().then((posts) => {
-        console.log("posts =>", posts);
+        //console.log("posts =>", posts);
+
         setPosts(posts);
       });
     });
   }, []);
 
-  return <>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</>;
+  return (
+    <>
+      {posts.length > 0 &&
+        posts.map((post) => <Post key={post._id} {...post} />)}
+    </>
+  );
 }
