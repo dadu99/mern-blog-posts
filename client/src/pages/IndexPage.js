35e7a3a3054ef859d35c04b5dd846f0089ed71be
Darius Baciu <baciu.darius01@gmail.com>
2024-04-09 14:27:00 +0300

    responsive css home blog page

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -14,8 +14,15 @@ export default function IndexPage() {
     });
   }, []);
 
+  const headingDay = {
+    color: "#37424c",
+    fontSize: "2xl",
+    textAlign: "center",
+    mt: "16px",
+  };
   return (
     <>
+      <h1 style={headingDay}>Have a nice day! </h1>
       {posts.length > 0 &&
         posts.map((post) => <Post key={post._id} {...post} />)}
     </>
